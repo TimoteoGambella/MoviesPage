5f04fead8de3ed33fa2e8c572b931e459d71b5a8
TimoteoGambella <timi.gambella@hotmail.com>
2022-05-31 15:25:27 -0300

    .

--- pages/_app.js
+++ pages/_app.js
@@ -17,8 +17,6 @@ function MyApp({ Component, pageProps }) {
 
       <Movies>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossOrigin="anonymous"/>
-          
-          <Script src='ConectorPlugin.js'/>
 
         <Component {...pageProps} />
       </Movies>
