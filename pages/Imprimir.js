4228a4e73b28f1ba1a5a14c875d6141cdfeb3c66
TimoteoGambella <timi.gambella@hotmail.com>
2022-05-31 13:23:29 -0300

    .

---
+++ pages/Imprimir.js
@@ -0,0 +1,10 @@
+import Header from "./Components/header/Header";
+
+export default function Favs(){
+    return(
+        <div>
+            <Header/>
+            <h1>IMPRIMIR</h1>
+        </div>
+    )
+}
