637311a3b36fb665ebc7a2459778cdf9ab4fafda
TimoteoGambella <timi.gambella@hotmail.com>
2022-06-06 18:56:26 -0300

    .

--- pages/Components/header/Navbar.js
+++ pages/Components/header/Navbar.js
@@ -18,41 +18,34 @@ export default function Nav_Bar({active}){
 
     return(
         <div className="navbar-container">
-            <Navbar expand="sl" className="navbar-expand-custom navBarPrincipal">
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="navbar-nav">
-                        {active==="Home"&&
-                            <>
-                                <p className="nav-link active" onClick={()=>handleHome("/Home")}>Inicio</p>
-                                <p href={'/Favs'} className="nav-link" onClick={()=>handleHome("/Favs")}>Favoritos</p>
-                                <p href={'/Cuenta'} className="nav-link" onClick={()=>handleHome("/Cuenta")}>Cuenta</p>
-                            </>
-                        }
-                        {active==="Favs"&&
-                            <>
-                                <p className="nav-link" onClick={()=>handleHome("/Home")}>Inicio</p>
-                                <p href={'/Favs'} className="nav-link active" onClick={()=>handleHome("/Favs")}>Favoritos</p>
-                                <p href={'/Cuenta'} className="nav-link" onClick={()=>handleHome("/Cuenta")}>Cuenta</p>
-                            </>
-                        }
-                        {active==="Cuenta"&&
-                            <>
-                                <p className="nav-link" onClick={()=>handleHome("/Home")}>Inicio</p>
-                                <p href={'/Favs'} className="nav-link" onClick={()=>handleHome("/Favs")}>Favoritos</p>
-                                <p href={'/Cuenta'} className="nav-link active" onClick={()=>handleHome("/Cuenta")}>Cuenta</p>
-                            </>
-                        }
-                        {active===undefined&&
-                            <>
-                                <p className="nav-link" onClick={()=>handleHome("/Home")}>Inicio</p>
-                                <p href={'/Favs'} className="nav-link" onClick={()=>handleHome("/Favs")}>Favoritos</p>
-                                <p href={'/Cuenta'} className="nav-link" onClick={()=>handleHome("/Cuenta")}>Cuenta</p>
-                            </>
-                        }
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            {active==="Home"&&
+                <>
+                    <p className="nav-link active" onClick={()=>handleHome("/Home")}>Inicio</p>
+                    <p href={'/Favs'} className="nav-link" onClick={()=>handleHome("/Favs")}>Favoritos</p>
+                    <p href={'/Cuenta'} className="nav-link" onClick={()=>handleHome("/Cuenta")}>Cuenta</p>
+                </>
+            }
+            {active==="Favs"&&
+                <>
+                    <p className="nav-link" onClick={()=>handleHome("/Home")}>Inicio</p>
+                    <p href={'/Favs'} className="nav-link active" onClick={()=>handleHome("/Favs")}>Favoritos</p>
+                    <p href={'/Cuenta'} className="nav-link" onClick={()=>handleHome("/Cuenta")}>Cuenta</p>
+                </>
+            }
+            {active==="Cuenta"&&
+                <>
+                    <p className="nav-link" onClick={()=>handleHome("/Home")}>Inicio</p>
+                    <p href={'/Favs'} className="nav-link" onClick={()=>handleHome("/Favs")}>Favoritos</p>
+                    <p href={'/Cuenta'} className="nav-link active" onClick={()=>handleHome("/Cuenta")}>Cuenta</p>
+                </>
+            }
+            {active===undefined&&
+                <>
+                    <p className="nav-link" onClick={()=>handleHome("/Home")}>Inicio</p>
+                    <p href={'/Favs'} className="nav-link" onClick={()=>handleHome("/Favs")}>Favoritos</p>
+                    <p href={'/Cuenta'} className="nav-link" onClick={()=>handleHome("/Cuenta")}>Cuenta</p>
+                </>
+            }
         </div>
     )
 }
